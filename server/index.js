5c54d14191c812a61c62946084987b99254d0234
Sachin Jayadev <sachinjayadev18@gmail.com>
2024-10-06 10:49:59 +0530

    ..

--- server/index.js
+++ server/index.js
@@ -14,13 +14,12 @@ dotenv.config({ path: "../.env" });
 const corsOptions = {
     origin: 'https://safe-frontend-swart.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    // allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 };
 
 app.use(cors(corsOptions));
-app.options('/auth/signup', cors(corsOptions));
-// app.options('*', cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use('/', (req, res, next) => {
     res.send('Hello from the root route!');
