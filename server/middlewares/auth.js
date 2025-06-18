18839bbde5b1ce31a8245aa44d4291edfbb3d5ed
Sachin Jayadev <sachinjayadev18@gmail.com>
2025-06-18 15:23:02 +0400

    removed logs

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -8,9 +8,9 @@ const verifyToken = (req, res, next) => {
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(401).json({ message: 'Unauthorized access' });
     }
-    console.log(authHeader);
+    // console.log(authHeader);
     const token = authHeader.split(' ')[1];
-    console.log(token);
+    // console.log(token);
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.userId = decoded.userId;
