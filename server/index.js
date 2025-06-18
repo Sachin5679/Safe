fb43a80de676782cf93d35d9040638b71d8dd15d
Sachin Jayadev <sachinjayadev18@gmail.com>
2025-06-18 11:23:53 +0400

    updated origins

--- server/index.js
+++ server/index.js
@@ -18,13 +18,7 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
